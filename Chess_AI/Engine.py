3a4a6e69dc9f4e7ca85146eb3256ba6c83a1c004
Kamil Zubko <kaminik@interia.pl>
2021-03-29 15:29:18 +0200

    Piece

--- Chess_AI/Engine.py
+++ Chess_AI/Engine.py
@@ -5,3 +5,69 @@ class ChessboardState:
         self.board = None   # TODO: Determine the way to store info (piece/square - oriented)
         self.white_to_move = True
         self.moveLog = []
+
+
+class Piece:
+    def __init__(self, position, color):
+        self.position = position
+        self.color = color
+
+
+class King(Piece):
+    def __init__(self, position, image):
+        super().__init__(position)
+        self.image = image
+
+    def possible_moves(self):
+        pass
+
+    def move(self, new_position):
+        self.position = new_position
+
+
+class Bishop(Piece):
+    def __init__(self, position, image):
+        super().__init__(position)
+        self.image = image
+
+    def possible_moves(self):
+        pass
+
+    def move(self, new_position):
+        self.position = new_position
+
+
+class Rook(Piece):
+    def __init__(self, position, image):
+        super().__init__(position)
+        self.image = image
+
+    def possible_moves(self):
+        pass
+
+    def move(self, new_position):
+        self.position = new_position
+
+
+class Knight(Piece):
+    def __init__(self, position, image):
+        super().__init__(position)
+        self.image = image
+
+    def possible_moves(self):
+        pass
+
+    def move(self, new_position):
+        self.position = new_position
+
+
+class Pawn(Piece):
+    def __init__(self, position, image):
+        super().__init__(position)
+        self.image = image
+
+    def possible_moves(self):
+        pass
+
+    def move(self, new_position):
+        self.position = new_position
