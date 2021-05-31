561796863c8b920ad4fd8f47ab4641c78a59daf5
Byczong <piotrzwsln8@gmail.com>
2021-05-31 02:10:11 +0200

    Implement swapping colors
    Orientation does not change (no plans on that)

--- Chess_AI/main.py
+++ Chess_AI/main.py
@@ -11,7 +11,7 @@ BORDER_WIDTH = 32
 WIDTH, HEIGHT = CHESSBOARD_WIDTH + BORDER_WIDTH, CHESSBOARD_HEIGHT + BORDER_WIDTH
 SCREEN = p.display.set_mode((WIDTH, HEIGHT))
 SQUARE_SIZE = CHESSBOARD_HEIGHT // 8
-FPS = 30
+FPS = 12
 
 BUTTON_WIDTH, BUTTON_HEIGHT = WIDTH // 3, HEIGHT // 8
 BUTTONS_X = (WIDTH // 2 - BUTTON_WIDTH // 2, WIDTH // 2 + BUTTON_WIDTH // 2)
@@ -20,11 +20,18 @@ BUTTON_2_Y = (HEIGHT // 8 - BUTTON_HEIGHT // 2 + HEIGHT // 4, HEIGHT // 8 + BUTT
 BUTTON_3_Y = (HEIGHT // 8 - BUTTON_HEIGHT // 2 + HEIGHT // 2, HEIGHT // 8 + BUTTON_HEIGHT // 2 + HEIGHT // 2)
 BUTTON_4_Y = (HEIGHT // 8 - BUTTON_HEIGHT // 2 + 3 * HEIGHT // 4, HEIGHT // 8 + BUTTON_HEIGHT // 2 + 3 * HEIGHT // 4)
 
+RECT_COLORS_WIDTH, RECT_COLORS_HEIGHT = WIDTH // 16, (BUTTON_3_Y[1] - BUTTON_3_Y[0]) // 2
+RECT_COLOR_A_X = (BUTTONS_X[1] + WIDTH // 32, BUTTONS_X[1] + WIDTH // 32 + RECT_COLORS_WIDTH)
+RECT_COLOR_A_Y = (BUTTON_3_Y[0], BUTTON_3_Y[0] + RECT_COLORS_HEIGHT)
+RECT_COLOR_B_X = (BUTTONS_X[1] + WIDTH // 32, BUTTONS_X[1] + WIDTH // 32 + RECT_COLORS_WIDTH)
+RECT_COLOR_B_Y = (RECT_COLOR_A_Y[1], RECT_COLOR_A_Y[1] + RECT_COLORS_HEIGHT)
+
 COLOR_LIGHT = p.Color((255, 235, 205))
 COLOR_DARK = p.Color((140, 80, 42))
 COLOR_HIGHLIGHT = p.Color((53, 180, 159))
 COLOR_CHECK = p.Color((200, 40, 65))
 COLOR_STALEMATE = p.Color((50, 180, 57))
+COLOR_BORDER = p.Color(173, 116, 59)
 
 BOARD_RANKS = ['1', '2', '3', '4', '5', '6', '7', '8']
 BOARD_FILES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
@@ -44,6 +51,7 @@ def main():
     tiles_clicked_on = []
     king_pos = None
     player_is_white = True
+    first_white_ai_move_made = False
 
     chessboard_state = Engine.ChessboardState()
     play_against_ai = False
@@ -56,7 +64,8 @@ def main():
         possible_moves = []
 
     def init_game(is_fist_init):
-        nonlocal running, quit_after_loop, play_against_ai, position, player_is_white, chessboard_state
+        nonlocal running, quit_after_loop, play_against_ai, position, \
+            player_is_white, chessboard_state, first_white_ai_move_made
         while running:
             clock.tick(FPS)
             for e in p.event.get():
@@ -73,12 +82,12 @@ def main():
                                 running = False
                                 play_against_ai = True
                             elif BUTTON_3_Y[0] <= position[1] <= BUTTON_3_Y[1]:
-                                # TODO: Change color (along with the orientation - white/black on the bottom)
-                                player_is_white = True
+                                player_is_white = not player_is_white
+
                             elif BUTTON_4_Y[0] <= position[1] <= BUTTON_4_Y[1]:
                                 quit_after_loop = True
                                 running = False
-            draw_init_window()
+            draw_init_window(player_is_white)
             p.display.flip()
 
         if quit_after_loop and is_fist_init:
@@ -88,7 +97,30 @@ def main():
         else:
             running = True
             SCREEN.fill(COLOR_DARK)
+            first_white_ai_move_made = False
             chessboard_state = Engine.ChessboardState()
+
+    def make_ai_move():
+        nonlocal chessboard_state, king_pos
+        ai_move = Engine.ChessAI(chessboard_state).ai_move(3)
+        chessboard_state.board[ai_move[0][0]][ai_move[0][1]].move(ai_move[1])
+        king_pos = chessboard_state.white_king.position if chessboard_state.white_to_move \
+            else chessboard_state.black_king.position
+        draw_chessboard_state(chessboard_state, selected_tile, possible_moves, king_pos)
+        p.display.flip()
+
+        print(f"[AIMove]: {get_tile_str(ai_move[0])} --> {get_tile_str(ai_move[1])}")
+        if chessboard_state.game_state() == Engine.GameState.CHECK:
+            print("[GameState]: Check")
+        elif chessboard_state.game_state() == Engine.GameState.CHECKMATE:
+            print("[GameState]: Checkmate")
+        elif chessboard_state.game_state() == Engine.GameState.STALEMATE:
+            print("[GameState]: Stalemate")
+        elif chessboard_state.game_state() == Engine.GameState.CONTINUE:
+            print("[GameState]: Continue")
+        elif chessboard_state.game_state() == Engine.GameState.INSUFFICIENT_MATERIAL:
+            print("[GameState]: Insufficient material")
+
 
     # Initial window
     init_game(True)
@@ -99,11 +131,17 @@ def main():
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
+
             elif event.type == p.KEYDOWN:
                 if event.key == p.K_ESCAPE:
                     print("------------")
                     print("Go back to the menu")
                     init_game(False)
+
+            if play_against_ai and not player_is_white and not first_white_ai_move_made:
+                make_ai_move()
+                first_white_ai_move_made = True
+
             elif event.type == p.MOUSEBUTTONDOWN:
                 position = p.mouse.get_pos()
 
@@ -149,7 +187,8 @@ def main():
                                 king_pos = chessboard_state.white_king.position if chessboard_state.white_to_move \
                                     else chessboard_state.black_king.position
 
-                                print(f"[HumanMove]: {get_tile_str(tiles_clicked_on[0])} --> {get_tile_str(tiles_clicked_on[1])}")
+                                print(
+                                    f"[HumanMove]: {get_tile_str(tiles_clicked_on[0])} --> {get_tile_str(tiles_clicked_on[1])}")
                                 if chessboard_state.game_state() == Engine.GameState.CHECK:
                                     print("[GameState]: Check")
                                 elif chessboard_state.game_state() == Engine.GameState.CHECKMATE:
@@ -161,7 +200,7 @@ def main():
                                 elif chessboard_state.game_state() == Engine.GameState.INSUFFICIENT_MATERIAL:
                                     print("[GameState]: Insufficient material")
 
-                                # TODO: Cool sound when taking pieces, less cool when not
+                                # TODO: Cool sound when taking pieces, less cool when not ???
                                 reset_move_attempt()
                             # First position selected v2
                             else:
@@ -178,25 +217,8 @@ def main():
         draw_chessboard_state(chessboard_state, selected_tile, possible_moves, king_pos)
         p.display.flip()
 
-        if play_against_ai and (player_is_white and not chessboard_state.white_to_move):
-            ai_move = Engine.ChessAI(chessboard_state).ai_move(3)
-            chessboard_state.board[ai_move[0][0]][ai_move[0][1]].move(ai_move[1])
-            king_pos = chessboard_state.white_king.position if chessboard_state.white_to_move \
-                else chessboard_state.black_king.position
-            draw_chessboard_state(chessboard_state, selected_tile, possible_moves, king_pos)
-            p.display.flip()
-
-            print(f"[AIMove]: {get_tile_str(ai_move[0])} --> {get_tile_str(ai_move[1])}")
-            if chessboard_state.game_state() == Engine.GameState.CHECK:
-                print("[GameState]: Check")
-            elif chessboard_state.game_state() == Engine.GameState.CHECKMATE:
-                print("[GameState]: Checkmate")
-            elif chessboard_state.game_state() == Engine.GameState.STALEMATE:
-                print("[GameState]: Stalemate")
-            elif chessboard_state.game_state() == Engine.GameState.CONTINUE:
-                print("[GameState]: Continue")
-            elif chessboard_state.game_state() == Engine.GameState.INSUFFICIENT_MATERIAL:
-                print("[GameState]: Insufficient material")
+        if play_against_ai and (player_is_white != chessboard_state.white_to_move):
+            make_ai_move()
 
 
 # Allow accessing a piece's image by PIECE['c#'], where c - color, # - abr. piece's name
@@ -223,8 +245,8 @@ def draw_chessboard():
             p.draw.rect(SCREEN, get_tile_color(row, col),
                         p.Rect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-    p.draw.line(SCREEN, p.Color(173, 116, 59), (8 * SQUARE_SIZE, 0), (8 * SQUARE_SIZE, 8 * SQUARE_SIZE), width=3)
-    p.draw.line(SCREEN, p.Color(173, 116, 59), (0, 8 * SQUARE_SIZE), (8 * SQUARE_SIZE, 8 * SQUARE_SIZE), width=3)
+    p.draw.line(SCREEN, COLOR_BORDER, (8 * SQUARE_SIZE, 0), (8 * SQUARE_SIZE, 8 * SQUARE_SIZE), width=3)
+    p.draw.line(SCREEN, COLOR_BORDER, (0, 8 * SQUARE_SIZE), (8 * SQUARE_SIZE, 8 * SQUARE_SIZE), width=3)
     font = p.font.SysFont("monospace", 24)
     for row in range(8):
         label = font.render(get_rank(row), True, COLOR_LIGHT)
@@ -284,16 +306,23 @@ def draw_pieces(chessboard_state):
                             p.Rect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
-def draw_init_window():
+def draw_init_window(top_is_white=True):
     """Draw initial window with buttons"""
     SCREEN.fill(COLOR_LIGHT)
-    texts = ["Play alone", "Play against AI", "Change color", "Quit"]
+    texts = ["Play alone", "Play against AI", "Swap colors", "Quit"]
     font = p.font.SysFont("monospace", 18)
     labels = [font.render(text, True, COLOR_LIGHT) for text in texts]
-    labels_rects = [labels[0].get_rect(center=(WIDTH // 2, HEIGHT / 8)),
-                    labels[1].get_rect(center=(WIDTH // 2, 3 * HEIGHT / 8)),
-                    labels[2].get_rect(center=(WIDTH // 2, 5 * HEIGHT / 8)),
-                    labels[3].get_rect(center=(WIDTH // 2, 7 * HEIGHT / 8))]
+    labels_rects = [labels[0].get_rect(center=(WIDTH // 2, HEIGHT // 8)),
+                    labels[1].get_rect(center=(WIDTH // 2, 3 * HEIGHT // 8)),
+                    labels[2].get_rect(center=(WIDTH // 2, 5 * HEIGHT // 8)),
+                    labels[3].get_rect(center=(WIDTH // 2, 7 * HEIGHT // 8))]
+
+    swap_colors_texts = ["You", "AI"]
+    swap_colors_labels = [font.render(text, True, p.Color(119, 136, 153)) for text in swap_colors_texts]
+    swap_colors_labels_rects = [swap_colors_labels[0].get_rect(center=((RECT_COLOR_A_X[0] + RECT_COLOR_A_X[1]) // 2,
+                                                                       (RECT_COLOR_A_Y[0] + RECT_COLOR_A_Y[1]) // 2)),
+                                swap_colors_labels[1].get_rect(center=((RECT_COLOR_B_X[0] + RECT_COLOR_B_X[1]) // 2,
+                                                                       (RECT_COLOR_B_Y[0] + RECT_COLOR_B_Y[1]) // 2))]
 
     p.draw.rect(SCREEN, COLOR_DARK, p.Rect(BUTTONS_X[0], BUTTON_1_Y[0], BUTTON_WIDTH, BUTTON_HEIGHT))
     SCREEN.blit(labels[0], labels_rects[0])
@@ -303,6 +332,29 @@ def draw_init_window():
 
     p.draw.rect(SCREEN, COLOR_DARK, p.Rect(BUTTONS_X[0], BUTTON_3_Y[0], BUTTON_WIDTH, BUTTON_HEIGHT))
     SCREEN.blit(labels[2], labels_rects[2])
+
+    if top_is_white:
+        p.draw.rect(SCREEN, p.Color(255, 255, 255), p.Rect(RECT_COLOR_A_X[0], RECT_COLOR_A_Y[0],
+                                                           RECT_COLORS_WIDTH, RECT_COLORS_HEIGHT))
+        p.draw.rect(SCREEN, p.Color(0, 0, 0), p.Rect(RECT_COLOR_B_X[0], RECT_COLOR_B_Y[0],
+                                                     RECT_COLORS_WIDTH, RECT_COLORS_HEIGHT))
+    else:
+        p.draw.rect(SCREEN, p.Color(0, 0, 0), p.Rect(RECT_COLOR_A_X[0], RECT_COLOR_A_Y[0],
+                                                     RECT_COLORS_WIDTH, RECT_COLORS_HEIGHT))
+        p.draw.rect(SCREEN, p.Color(255, 255, 255), p.Rect(RECT_COLOR_B_X[0], RECT_COLOR_B_Y[0],
+                                                           RECT_COLORS_WIDTH, RECT_COLORS_HEIGHT))
+
+    SCREEN.blit(swap_colors_labels[0], swap_colors_labels_rects[0])
+    SCREEN.blit(swap_colors_labels[1], swap_colors_labels_rects[1])
+
+    p.draw.line(SCREEN, COLOR_BORDER, (RECT_COLOR_A_X[0], RECT_COLOR_A_Y[0]),
+                (RECT_COLOR_A_X[1], RECT_COLOR_A_Y[0]), width=2)
+    p.draw.line(SCREEN, COLOR_BORDER, (RECT_COLOR_B_X[0], RECT_COLOR_B_Y[1]),
+                (RECT_COLOR_B_X[1], RECT_COLOR_B_Y[1]), width=2)
+    p.draw.line(SCREEN, COLOR_BORDER, (RECT_COLOR_A_X[0], RECT_COLOR_A_Y[0]),
+                (RECT_COLOR_A_X[0], RECT_COLOR_B_Y[1]), width=2)
+    p.draw.line(SCREEN, COLOR_BORDER, (RECT_COLOR_A_X[1], RECT_COLOR_A_Y[0]),
+                (RECT_COLOR_A_X[1], RECT_COLOR_B_Y[1]), width=2)
 
     p.draw.rect(SCREEN, COLOR_DARK, p.Rect(BUTTONS_X[0], BUTTON_4_Y[0], BUTTON_WIDTH, BUTTON_HEIGHT))
     SCREEN.blit(labels[3], labels_rects[3])
